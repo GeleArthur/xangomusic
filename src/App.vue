33d88c1f8e04bb74412940664c91ce46c30764fd
arthur <geleminecrafter@gmail.com>
2021-12-10 15:23:27 +0100

    Vue 2

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <TopNavBar/>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TopNavBar from './components/TopNavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TopNavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TopNavBar.vue
@@ -0,0 +1,15 @@
+<template>
+
+<div>
+    <p>wow</p>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'TopNavBar',
+  components: {
+  }
+}
+</script>
