d579a9e401b78395b413c4400c9f5be5f22fd8dd
Appelsaus <ruben.p.t.meyer@gmail.com>
2022-01-19 16:49:20 +0100

    breakfix

--- src/components/album.vue
+++ src/components/album.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="grid-item">
-    <img src="xango logo 2012.png" />
+    <img src="xango logo 2012.png" /><br>
     Album Title<br>
     Group or Artist<br>
-    Country of Origin<br>
+    Country of Origin
   </div>
 </template>
 
