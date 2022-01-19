ef257f35d5de254a77fbb88b3274f273e4a4f41f
arthur <geleminecrafter@gmail.com>
2022-01-19 12:29:00 +0100

    Scss working

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="contents">
-      <div class="search">wow</div>
+      <div class="search">SEARCH BAR</div>
       <div class="topBar">
           <div>do</div>
           <div>do</div>
@@ -10,7 +10,7 @@
           <div>do</div>
           </div>
     </div>
-    <div class="icon">10</div>
+    <img class="icon" src="xango logo 2012.png" alt="xango logo">
   </div>
 </template>
 
@@ -18,7 +18,7 @@
 export default {};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .container {
   width: 100%;
   height: 9rem;
@@ -42,11 +42,14 @@ export default {};
 .topBar {
   display: grid;
   grid-template-columns: auto auto auto auto auto;
-  align-items: center;
-  text-align: center;
   grid-area: topBar;
 }
 .icon {
   grid-area: icon;
+  background-color: white;
+  display: block;
+  height: 100%;
+  // width: 100%;
+  // padding: 10%;
 }
 </style>
